b97054f886de47c5b5efd73b78bd7bd5ad03abbf
mnsc9202 <mnsc9202@gmail.com>
2023-03-20 11:24:07 +0900

    remove

--- practice_0001_sidebar/src/store/index.tsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { selectSideBarStore } from "./selectSideBarStore";
-
-// store
-export default configureStore({
-  reducer: {
-    selectSideBar: selectSideBarStore.reducer,
-  },
-});
